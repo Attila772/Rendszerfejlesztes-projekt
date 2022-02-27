90eb11bd54af989ab61690bfbee1388e98534bcb
Attila772 <71554727+Attila772@users.noreply.github.com>
2022-02-27 21:54:44 +0100

    alap commit
    alap backend file-ok mappák, flask hello world + frontend mappa

---
+++ backend/app.py
@@ -0,0 +1,16 @@
+#from api import create_app
+
+#app = create_app()
+
+#if __name__ == '__main__':
+#    app.run(debug=True)
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
