9ea76fdcbbea7db7a9d8abebd24ffa9fb3177adf
Attila772 <71554727+Attila772@users.noreply.github.com>
2022-03-12 15:27:11 +0100

    Update views.py
    /location

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,7 +2,7 @@ from multiprocessing.sharedctypes import Value
 from re import fullmatch
 from unicodedata import name
 from flask_login.utils import login_required
-from .model import User,level,trade
+from .model import User,level,trade,location
 from flask import Blueprint, jsonify, render_template, request, flash, redirect, url_for
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
@@ -33,6 +33,35 @@ def route():
     elif request.method =='PUT':
         id = request.get_json()['id']
         level.query.filter_by(id = id).first().name = request.get_json()['name']
+        db.session.commit()
+        return jsonify({'Data':'Sikeres'})
+    pass
+
+@views.route('/location', methods=['GET', 'POST','PUT','DELETE'])
+def _location():
+    if request.method == 'POST':
+        name = request.get_json()['building']
+        room = request.get_json()['rooms']
+        new_loc = location(building= name, rooms = room)
+        db.session.add(new_loc)
+        db.session.commit()
+        return jsonify({'Data':'Sikeres'})
+    elif request.method == 'GET':
+        locations = location.query.filter_by()
+        response_dict = {}
+        for _location in locations:
+            response_dict[_location.id] = [_location.building,_location.rooms]
+        response = jsonify({'Locations': response_dict})
+    elif request.method == 'DELETE':
+        id = request.get_json()['id']
+        _location = location.query.filter_by(id = id).first()
+        db.session.delete(_location)
+        db.session.commit()
+        return jsonify({'Data':'Sikeres'})
+    elif request.method =='PUT':
+        id = request.get_json()['id']
+        location.query.filter_by(id = id).first().building = request.get_json()['building']
+        location.query.filter_by(id = id).first().rooms = request.get_json()['room']
         db.session.commit()
         return jsonify({'Data':'Sikeres'})
     pass
