1da602e4351ea93ae5b6c7744d750bdc99b5e975
Attila772 <71554727+Attila772@users.noreply.github.com>
2022-03-14 13:21:33 +0100

    route-ok regisztralasa

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -21,9 +21,11 @@ def create_app():
 
     #from .views import views
     from .auth import auth
+    from .views import views
 
     #app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(views, url_prefix='/')
 
     from .model import User
     from .model import trade
