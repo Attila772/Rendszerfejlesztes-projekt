4d3a19caa570662d7b913526a018a3a73b4feb52
KaposiAron <kaposiaron6@gmail.com>
2022-03-29 10:44:58 +0200

    Log, Privilige level & Qualification api
    Log api: full
    Privilige level & Qualification api: modify, getById

--- frontend/maintenance/src/shared/network/privilige-level.api.ts
+++ frontend/maintenance/src/shared/network/privilige-level.api.ts
@@ -1,3 +1,4 @@
+import { Role } from "../../components/types";
 import { SERVER_ADDRESS } from "../common/constants";
 
 export type CreatePriviligeRequest = {
@@ -27,6 +28,16 @@ export const createPriviligeLevel = async (
   return response.json();
 };
 
+export const modifyPriviligeLevel = async (priviligeLevel: Role) => {
+  const requestOptions = {
+    method: "PUT",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify({ ...priviligeLevel }),
+  };
+  const response = await fetch(`${SERVER_ADDRESS}/role`, requestOptions);
+  return response.json();
+};
+
 export const deletePriviligeLevel = async (id: string | number) => {
   const requestOptions = {
     method: "DELETE",
@@ -34,5 +45,16 @@ export const deletePriviligeLevel = async (id: string | number) => {
     body: JSON.stringify({ id: id }),
   };
   const response = await fetch(`${SERVER_ADDRESS}/role`, requestOptions);
+  return response.json();
+};
+
+//get-by-id
+export const getPriviligeLevelById = async (id: string) => {
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify({ id: id }),
+  };
+  const response = await fetch(`${SERVER_ADDRESS}/role-by-id`, requestOptions);
   return response.json();
 };
